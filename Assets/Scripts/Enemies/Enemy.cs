f8865ad23b3f04619726f322b8a6b824486ff660
Rin <lvqq2000@gmail.com>
2024-08-17 17:18:17 +1000

    Resolve the conflict between commits

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -11,10 +11,19 @@ namespace Enemies
         private PlayerController target;
         //pathfinding
         private NavMeshAgent agent;
+
+        [SerializeField] private GameObject projectilePrefab;
+
+        [SerializeField] private float shootRate = 1f;
+
+        private float _nextShoot;
+
         private void Start()
         {
             target = FindObjectOfType<PlayerController>();
             agent = GetComponent<NavMeshAgent>();
+
+            _nextShoot = Time.time + shootRate;
         }
 
         void Update()
@@ -24,6 +33,28 @@ namespace Enemies
             {
                 agent.SetDestination(target.transform.position);
             }
+
+            if (Time.time > _nextShoot) {
+            // Update the time when the enemy can shoot next
+            _nextShoot = Time.time + shootRate;
+
+            Shoot();
+            }
         }
+
+        private void Shoot()
+    {
+        
+        //spawn a projectile
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+        //get the Projectile component from the projectile object
+        Projectile projectileComponent = projectile.GetComponent<Projectile>();
+        //check if the Projectile component exists
+        if (projectileComponent != null)
+        {
+            //set the initial direction of the projectile
+            projectileComponent.SetInitialDirection((target.transform.position - transform.position).normalized);
+        }
+    }
     }
 }
